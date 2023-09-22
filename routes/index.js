41d66a72d630ac5f60f7edd70136c5bc7ff4ef2d
koaladlt <manuelejdelatorre@gmail.com>
2023-09-22 18:02:01 -0300

    hot fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,61 @@
+var express = require("express");
+var router = express.Router();
+const puppeteer = require("puppeteer");
+
+let browser;
+let page;
+
 const maxRetries = 3;
-const retryDelay = 1000;
+const retryDelay = 1000; // Delay in milliseconds
+
+// Initialize Puppeteer, create a single page instance, and block unnecessary resources
+async function initializePuppeteer() {
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
+
+    page = await browser.newPage();
+
+    // Block unnecessary resources
+    await page.setRequestInterception(true);
+    page.on("request", (request) => {
+      if (
+        ["image", "stylesheet", "font", "media"].includes(
+          request.resourceType()
+        )
+      ) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+
+    await page.goto(process.env.DOLLAR_SOURCE);
+  } catch (error) {
+    console.error("Error initializing Puppeteer:", error);
+  }
+}
+
+initializePuppeteer();
+
+async function fetchData(selector) {
+  const value = await page.$eval(selector, (element) => element.textContent);
+  return value;
+}
 
 async function retryFetchData(selector) {
   for (let i = 0; i < maxRetries; i++) {
     try {
+      if (!page) {
+        console.log("Reinitializing Puppeteer");
+        await initializePuppeteer();
+      }
       return await fetchData(selector);
     } catch (error) {
       console.error(`Error fetching data (Attempt ${i + 1}):`, error);
@@ -39,3 +91,5 @@ router.get("/euro", async (req, res, next) => {
     res.status(500).send("Error fetching data");
   }
 });
+
+module.exports = router;
