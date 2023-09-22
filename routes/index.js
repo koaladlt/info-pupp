a4a52b5adc5129c67b35aa925e2c27e30d3804b6
koaladlt <manuelejdelatorre@gmail.com>
2023-09-22 17:24:30 -0300

    retry if fails

--- routes/index.js
+++ routes/index.js
@@ -1,69 +1,41 @@
-var express = require('express');
-var router = express.Router();
-const puppeteer = require('puppeteer');
+const maxRetries = 3;
+const retryDelay = 1000;
 
-let browser;
-let page;
-
-// Initialize Puppeteer, create a single page instance, and block unnecessary resources
-async function initializePuppeteer() {
-  browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath:
-      process.env.NODE_ENV === 'production'
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-
-  page = await browser.newPage();
-
-  // Block unnecessary resources
-  await page.setRequestInterception(true);
-  page.on('request', (request) => {
-    if (
-      ['image', 'stylesheet', 'font', 'media'].includes(request.resourceType())
-    ) {
-      request.abort();
-    } else {
-      request.continue();
+async function retryFetchData(selector) {
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      return await fetchData(selector);
+    } catch (error) {
+      console.error(`Error fetching data (Attempt ${i + 1}):`, error);
+      if (i < maxRetries - 1) {
+        // If not the last attempt, wait for a while before retrying
+        await new Promise((resolve) => setTimeout(resolve, retryDelay));
+      }
     }
-  });
-
-  await page.goto(process.env.DOLLAR_SOURCE); // Assuming DOLLAR_SOURCE and EURO_SOURCE are the same
+  }
+  throw new Error("Max retries reached");
 }
 
-initializePuppeteer();
-
-async function fetchData(selector) {
-  const value = await page.$eval(selector, (element) => element.textContent);
-  return value;
-}
-
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const value = await fetchData(
-      '#dolar > div > div > div.col-sm-6.col-xs-6.centrado > strong'
+    const value = await retryFetchData(
+      "#dolar > div > div > div.col-sm-6.col-xs-6.centrado > strong"
     );
-    console.log({ value });
     res.send(value);
   } catch (error) {
-    console.error('Error fetching dollar data:', error);
-    res.status(500).send('Error fetching data');
+    console.error("Error fetching dollar data:", error);
+    res.status(500).send("Error fetching data");
   }
 });
 
-router.get('/euro', async (req, res, next) => {
+router.get("/euro", async (req, res, next) => {
   try {
-    const value = await fetchData(
-      '#euro > div > div > div.col-sm-6.col-xs-6.centrado > strong'
+    const value = await retryFetchData(
+      "#euro > div > div > div.col-sm-6.col-xs-6.centrado > strong"
     );
-    console.log(value);
     res.send(value);
   } catch (error) {
-    console.error('Error fetching euro data:', error);
-    res.status(500).send('Error fetching data');
+    console.error("Error fetching euro data:", error);
+    res.status(500).send("Error fetching data");
   }
 });
-
-module.exports = router;
